b3e4dc69f632a3a3f064336898692c3c39e51af2
Andrew <lyerdestroyer@gmail.com>
2021-10-19 16:43:29 +0800

    Revert "Implementing Firebase Auth Native"
    This reverts commit af1d0652082633f71ea511e214afd1674d1cb907.

--- src/api/firebaseAuthNative.ts
+++
@@ -1,7 +0,0 @@
-import { FirebaseAuthentication } from '@ionic-native/firebase-authentication'
-
-export const getCurrentUser = async () => {
-    const currentUser = await FirebaseAuthentication.getCurrentUser()
-
-    return currentUser
-}
