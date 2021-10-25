ca92fcef09dcf133d2dd67a849436597edc77d3e
Aivee Kei Soriano <aiveekei.soriano@gmail.com>
2021-10-25 23:06:12 +0800

    location

--- src/components/DateAndLocation/index.tsx
+++ src/components/DateAndLocation/index.tsx
@@ -1,6 +1,10 @@
 import { IonGrid, IonRow, IonCol } from "@ionic/react"
 import MainClock from "../MainClock"
 import moment from "moment"
+import { useEffect, useState } from "react"
+
+import { Geolocation } from '@capacitor/geolocation';
+import { NativeGeocoder, NativeGeocoderOptions } from '@ionic-native/native-geocoder'
 
 const DateAndLocation: React.FC = () => {
 
@@ -18,23 +22,58 @@ const DateAndLocation: React.FC = () => {
         }
     }
 
-    return (
-        <IonGrid style={_styles.grid}>
-            <IonRow>
-                <IonCol class="ion-text-center" style={_styles.place}>
-                    Lucena
-                </IonCol>
-            </IonRow>
-            <IonRow>
-                <IonCol class="ion-text-center">
-                    {moment().format("DD MMMM YYYY")}
-                </IonCol>
-            </IonRow>
-            <IonRow style={_styles.clock}>
-                <MainClock />
-            </IonRow>
-        </IonGrid>
-    )
+    const [location, setLocation] = useState<any>()
+
+    // interface Position {
+    //     timestamp: number,
+    //     coords: { 
+    //         latitude: number,
+    //         longitude: number,
+    //         accuracy: number,
+    //         altitudeAccuracy: number | null,
+    //         altitude: number | null,
+    //         speed: number | null,
+    //         heading: number | null
+    //     }
+    // }
+
+
+    let options: NativeGeocoderOptions = {
+        useLocale: true,
+        maxResults: 5
+    };
+
+    const printCurrentPosition = async () => {
+        const coordinates = await Geolocation.getCurrentPosition();
+            const {latitude, longitude} = coordinates.coords
+            NativeGeocoder.reverseGeocode(latitude, longitude, options)
+                .then(res => {
+                    console.log('Current position:', res);
+                    setLocation(res[0].administrativeArea)
+                })
+  };
+
+  useEffect(() => {
+                printCurrentPosition()
+            }, [])
+
+            return (
+            <IonGrid style={_styles.grid}>
+                <IonRow>
+                    <IonCol class="ion-text-center" style={_styles.place}>
+                        {location}
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol class="ion-text-center">
+                        {moment().format("DD MMMM YYYY")}
+                    </IonCol>
+                </IonRow>
+                <IonRow style={_styles.clock}>
+                    <MainClock />
+                </IonRow>
+            </IonGrid>
+            )
 }
 
-export default DateAndLocation
+            export default DateAndLocation
